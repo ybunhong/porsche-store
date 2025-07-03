cafb20bb392c4c38cad9faf219333ab86ee6c7a0
Dararith <prakdararith22@gmail.com>
2025-07-03 15:16:34 +0700

    feat: add utility class and icon
    - add call to action svg
    - add typography utility aligntment color weight size
    - add margin as varable
    - add margin and padding utility
    - create new utility(background.css) and add the utility class

---
+++ src/assets/assets.js
@@ -0,0 +1,59 @@
+import PinterestIcon from "./callToAction/pinterest.svg";
+import ArrowOppositeIcon from "./callToAction/arrow-opposite.svg";
+import BellIcon from "./callToAction/bell.svg";
+import CardIcon from "./callToAction/card.svg";
+import ChevronIcon from "./callToAction/chevron.svg";
+import ChimneyIcon from "./callToAction/chimney.svg";
+import DurationIcon from "./callToAction/duration.svg";
+import EmailIcon from "./callToAction/email.svg";
+import FacebookIcon from "./callToAction/facebook.svg";
+import FileIcon from "./callToAction/file.svg";
+import Flowbite from "./callToAction/flowbite.svg";
+import GoogleIcon from "./callToAction/google.svg";
+import InformationIcon from "./callToAction/Information.svg";
+import InstagramIcon from "./callToAction/instagram.svg";
+import LeatherIcon from "./callToAction/leather.svg";
+import LinkedinIcon from "./callToAction/linkin.svg";
+import LocationIcon from "./callToAction/location.svg";
+import NegativeIcon from "./callToAction/Negative.svg";
+import PaperAirplaneIcon from "./callToAction/paper-airplane.svg";
+import PenIcon from "./callToAction/pen.svg";
+import PlusIcon from "./callToAction/Plus.svg";
+import SearchIcon from "./callToAction/Search.svg";
+import TagIcon from "./callToAction/tag.svg";
+import TruckIcon from "./callToAction/truck.svg";
+import UpDownArrowIcon from "./callToAction/up-down-arrows.svg";
+import VerifyCheckIcon from "./callToAction/verify-check.svg";
+import XIcon from "./callToAction/x.svg";
+import YoutubeIcon from "./callToAction/Youtube.svg";
+
+export {
+  PinterestIcon,
+  ArrowOppositeIcon,
+  BellIcon,
+  CardIcon,
+  ChevronIcon,
+  ChimneyIcon,
+  DurationIcon,
+  EmailIcon,
+  FacebookIcon,
+  FileIcon,
+  Flowbite,
+  GoogleIcon,
+  InformationIcon,
+  InstagramIcon,
+  LeatherIcon,
+  LinkedinIcon,
+  LocationIcon,
+  NegativeIcon,
+  PaperAirplaneIcon,
+  PenIcon,
+  PlusIcon,
+  SearchIcon,
+  TagIcon,
+  TruckIcon,
+  UpDownArrowIcon,
+  VerifyCheckIcon,
+  XIcon,
+  YoutubeIcon,
+};
