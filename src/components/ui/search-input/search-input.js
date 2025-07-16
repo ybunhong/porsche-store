ec3005a6e332239004f540c77a76039a325f425c
Dararith <prakdararith22@gmail.com>
2025-07-16 10:05:46 +0700

    add(search-panel): create new search input component with functionality

---
+++ src/components/ui/search-input/search-input.js
@@ -0,0 +1,48 @@
+import "./search-input.css";
+import "@ui";
+import { search } from "@assets";
+
+class searchInput extends BaseComponent {
+  static get observedAttributes() {
+    return ["text"];
+  }
+
+  constructor() {
+    super();
+    this.text = "";
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.action = this.getAttribute("action");
+    this.updateTemplate();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      switch (name) {
+        case "text":
+          this.text = newValue || "";
+          break;
+        default:
+          console.warn(`Unhandled observed attribute: ${name}`);
+          break;
+      }
+      this.updateTemplate();
+    }
+  }
+
+  updateTemplate() {
+    this.template = `
+    <div class="search-input flex items-center px-3 border border-medium rounded-sm w-full">
+      <input type="text" placeholder="${this.text}" 
+      class="outline-none border-none body-sm w-full"/>
+      <icon-button icon="${search}"><icon-button>
+    </div>
+    `;
+
+    this.render();
+  }
+}
+
+customElements.define("search-input", searchInput);
