cf33e62f2628f1057a9bd9e96995802969b90abd
Dararith <prakdararith22@gmail.com>
2025-07-19 11:53:57 +0700

    add(base button): add the event emitter and whitespace nowrap to base button

--- src/components/ui/base-button/base-button.js
+++ src/components/ui/base-button/base-button.js
@@ -2,7 +2,7 @@ import "./base-button.css";
 
 class BaseButton extends BaseComponent {
   static get observedAttributes() {
-    return ["label", "variant", "disabled", "type"];
+    return ["label", "variant", "disabled", "type", "action"];
   }
 
   constructor() {
@@ -16,32 +16,59 @@ class BaseButton extends BaseComponent {
   connectedCallback() {
     super.connectedCallback();
     this.updateTemplate();
+    this.addEventListener("click", event => {
+      if (this.disabled) {
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+      }
+      if (this.action) {
+        this.dispatchEvent(
+          new CustomEvent("base-button", {
+            bubbles: true,
+            detail: { action: this.action },
+          })
+        );
+      }
+    });
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (oldValue !== newValue) {
-      if (name === "label") {
-        this.label = newValue;
-      }
-      if (name === "variant") {
-        this.variant = newValue;
-      }
-      if (name === "type") {
-        this.type = newValue;
+      switch (name) {
+        case "label":
+          this.label = newValue;
+          break;
+        case "variant":
+          this.variant = newValue;
+          break;
+        case "type":
+          this.type = newValue;
+          break;
+        case "action":
+          this.action = newValue;
+          break;
+        case "disabled":
+          this.disabled = newValue !== null && newValue !== "false";
+          break;
+        default: {
+          console.warn(`Unhandled observed attribute: ${name}`);
+          break;
+        }
       }
       this.updateTemplate();
     }
   }
 
   updateTemplate() {
-    let classes = "px-2 rounded-sm  text-center w-full ";
+    let classes = " rounded-sm  text-center w-full whitespace-nowrap ";
 
     classes += `button-${this.variant} `;
 
     if (this.type === "button") {
-      classes += "py-3 heading-lg ";
+      classes += "py-3 px-4 heading-xs";
     } else if (this.type === "status") {
-      classes += "py-1 body-3xs ";
+      classes += "py-1  px-2 body-3xs ";
     }
 
     this.template = `
