18b24b2ee0e52e30780ef6d57b62fd32b14ec630
QuietLamb <chhunchannygod@gmail.com>
2025-07-21 17:04:15 +0700

    fix: resolved overide class bug

--- src/components/ui/category-card/category-card.js
+++ src/components/ui/category-card/category-card.js
@@ -26,7 +26,7 @@ class CategoryCard extends BaseComponent {
 
   render() {
     this.innerHTML = `
-        <div class="card flex relative w-full h-full rounded-lg overflow-hidden">
+        <div class="cards flex relative w-full h-full rounded-lg overflow-hidden">
           <img class="card-image w-full h-full relative top-0 left-0 bottom-0 z-0" alt="card-image" />
           <div class="text-area w-full h-fit flex absolute top-0 left-0 z-100 p-3">
             <div class="text w-fit h-fit">
