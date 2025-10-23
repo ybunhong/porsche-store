6663e9aa72f4c95c083ee104a24ec99acf8a1f19
ybunhong <ybunhon369@gmail.com>
2025-10-24 05:12:29 +0700

    fix: image path

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,8 @@ export default defineConfig({
     assetsDir: "assets",
     rollupOptions: {
       output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
         assetFileNames: "assets/[name].[ext]",
       },
     },
