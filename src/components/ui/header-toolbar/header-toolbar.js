b87d4696446e946c93d113424dcaead6f5832e5c
ybunhong <ybunhon369@gmail.com>
2025-10-24 07:38:59 +0700

    fix: headertoolbar assets

--- src/components/ui/header-toolbar/header-toolbar.js
+++ src/components/ui/header-toolbar/header-toolbar.js
@@ -1,5 +1,5 @@
 import "./header-toolbar.css";
-import { cartEmpty, menu, profile, search, porscheIcon, porscheText, heart } from "@assets";
+import { cartEmpty, menu, profile, search, porscheIcon, porscheText, heart } from "../../../assets/assets.js";
 import { BaseComponent } from "../../base-component.js";
 import "@ui";
 import "@layout";
